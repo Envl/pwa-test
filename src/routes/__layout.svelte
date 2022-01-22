b1446f9dd1816786c28ac9c5428b603718a13bd4
envl <yyaomingm@gmail.com>
2022-01-23 01:28:59 +0800

    init

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { browser, dev } from '$app/env';
+</script>
+
+<svelte:head>
+	{#if !dev && browser}
+		<link rel="manifest" href="/manifest.webmanifest" />
+	{/if}
+</svelte:head>
+<slot><!-- optional fallback --></slot>
